19c0bbb2937fec616ce62386a37187d5070101b5
huany267 <103295659+huany267@users.noreply.github.com>
2022-04-08 21:48:42 -0400

    Add files via upload

---
+++ course.c
@@ -0,0 +1,99 @@
+#include "course.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * @brief This is the information about erolling students.
+ * 
+ * @param course 
+ * @param student 
+ * @return nothing
+ */
+ 
+void enroll_student(Course *course, Student *student)
+{
+  course->total_students++;
+  if (course->total_students == 1) 
+  {
+    course->students = calloc(1, sizeof(Student));
+  }
+  else 
+  {
+    course->students = 
+      realloc(course->students, course->total_students * sizeof(Student)); 
+  }
+  course->students[course->total_students - 1] = *student;
+}
+/**
+ * @brief The function gives the information about the name, code, and the total students.
+ * @return nothing
+ * 
+ * @param course 
+ */
+void print_course(Course* course)
+{
+  printf("Name: %s\n", course->name);
+  printf("Code: %s\n", course->code);
+  printf("Total students: %d\n\n", course->total_students);
+  printf("****************************************\n\n");
+  for (int i = 0; i < course->total_students; i++) 
+    print_student(&course->students[i]);
+}
+/**
+ * @brief gives the info about the top student in the class.
+ * 
+ * @param course 
+ * @return Student* 
+ */
+
+Student* top_student(Course* course)
+{
+  if (course->total_students == 0) return NULL;
+ 
+  double student_average = 0;
+  double max_average = average(&course->students[0]);
+  Student *student = &course->students[0];
+ 
+  for (int i = 1; i < course->total_students; i++)
+  {
+    student_average = average(&course->students[i]);
+    if (student_average > max_average) 
+    {
+      max_average = student_average;
+      student = &course->students[i];
+    }   
+  }
+
+  return student;
+}
+/**
+ * @brief gives the info about the students that pass the course
+ * 
+ * @param course 
+ * @param total_passing 
+ * @return Student* 
+ */
+
+Student *passing(Course* course, int *total_passing)
+{
+  int count = 0;
+  Student *passing = NULL;
+  
+  for (int i = 0; i < course->total_students; i++) 
+    if (average(&course->students[i]) >= 50) count++;
+  
+  passing = calloc(count, sizeof(Student));
+
+  int j = 0;
+  for (int i = 0; i < course->total_students; i++)
+  {
+    if (average(&course->students[i]) >= 50)
+    {
+      passing[j] = course->students[i];
+      j++; 
+    }
+  }
+
+  *total_passing = count;
+
+  return passing;
+}
---
+++ main.c
@@ -0,0 +1,39 @@
+
+#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+#include "time.h"
+#include "course.h"
+/**
+ * @brief calloc is being used in this function to allocate the memory that will eventually return a void pointer to the memory.
+ * @param course
+ * @param student
+ * @return int 
+ */
+
+int main()
+{
+  srand((unsigned) time(NULL));
+
+  Course *MATH101 = calloc(1, sizeof(Course));
+  strcpy(MATH101->name, "Basics of Mathematics");
+  strcpy(MATH101->code, "MATH 101");
+
+  for (int i = 0; i < 20; i++) 
+    enroll_student(MATH101, generate_random_student(8));
+  
+  print_course(MATH101);
+
+  Student *student;
+  student = top_student(MATH101);
+  printf("\n\nTop student: \n\n");
+  print_student(student);
+
+  int total_passing;
+  Student *passing_students = passing(MATH101, &total_passing);
+  printf("\nTotal passing: %d\n", total_passing);
+  printf("\nPassing students:\n\n");
+  for (int i = 0; i < total_passing; i++) print_student(&passing_students[i]);
+  
+  return 0;
+}
